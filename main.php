e93a2d7baea080f1c0a9841912946bb6a36a636d
NayaTK-NP <herenfuyong02@gmail.com>
2024-06-20 14:07:51 +0900

    feat: initial commit

---
+++ main.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <title></title>
+    <link href="main.css" rel="stylesheet">
+    <meta name="viewport" content="width=device-width,initial-scale=1" />
+    </meta>    
+</head>
+
+<body>
+    <div class="content">
+        <header>
+            <a class="header" href="/"></a>
+        </header>            
+
+        <footer>
+            &copy; 2024 Co+Work13
+        </footer>
+    </div>
+</body>
+</html>
