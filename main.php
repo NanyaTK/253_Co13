2d4fe4cbb16dc7f1aa76ec0f51334f89ad4b0868
NayaTK-NP <herenfuyong02@gmail.com>
2024-09-26 14:04:11 +0900

    feat

--- main.php
+++ main.php
@@ -6,18 +6,32 @@
     <title></title>
     <link href="main.css" rel="stylesheet">
     <meta name="viewport" content="width=device-width,initial-scale=1" />
-    </meta>    
+    </meta>
 </head>
 
 <body>
-    <div class="content">
+    <div class="container">
         <header>
-            <a class="header" href="/">ghjghjk</a>
-        </header>            
+            <h1>ゲーム: 背景写真</h1>
+        </header>
 
-        <footer>
-            &copy; 2024 Co+Work13
-        </footer>
+        <div class="flex">
+            <section class="how-to-play">
+                <h2>How to Play</h2>
+                <p>ゲームのプレイ方法をここに説明します。</p>
+            </section>
+
+            <section class="download">
+                <h2>How to Make</h2>
+                <p>ダウンロードリンクを貼る</p>
+            </section>
+        </div>
+
+        <section class="environment">
+            <h2>環境</h2>
+            <p>ここに環境に関する説明が入ります。</p>
+        </section>
     </div>
 </body>
+
 </html>
